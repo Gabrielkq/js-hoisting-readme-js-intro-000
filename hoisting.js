6c0bef3bb429fe052d956322a9c9d640c3784948
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-07-01 21:14:08 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -22,12 +22,12 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
+sayMy();
  
 
   function sayMy() {
     var name = "Kristin";
     console.log(name);
     
-  } sayMy();
+  } 
 }
